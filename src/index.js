e1c9b6a6e3884125396639ee743e17c1706de567
git-Sahitya <sahitya5289@gmail.com>
2024-10-13 17:17:07 +0530

    database Connection

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
+  } catch (error) {
+    console.log("Error:", error);
+  }
+})();
